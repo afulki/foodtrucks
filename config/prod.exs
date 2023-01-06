dae378a32a3271bb0793b926c8e2320aa684fd30
Gary Thom <gary.thom@afulki.com>
2023-01-06 15:25:19 -0500

    Fixing prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -47,3 +47,10 @@ config :logger, level: :info
 #       force_ssl: [hsts: true]
 #
 # Check `Plug.SSL` for all available options in `force_ssl`.
+
+config :foodtrucks, FoodtrucksWeb.Endpoint,
+  http: [port: {:system, "PORT"}], # Possibly not needed, but doesn't hurt
+  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443],
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
+  server: true
+
